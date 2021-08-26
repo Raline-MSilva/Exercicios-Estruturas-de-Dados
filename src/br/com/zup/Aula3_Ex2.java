f5886d47dbc823dc3e0bf2e90ac4b2c47ed619ba
Raline Silva <ralinesilva1@gmail.com>
2021-08-26 13:45:23 -0300

    primeiro commit

--- src/br/com/zup/Aula3_Ex2.java
+++ src/br/com/zup/Aula3_Ex2.java
@@ -18,8 +18,8 @@ public class Aula3_Ex2 {
         if (produto1 < produto2 & produto1 < produto3){
             System.out.println("Produto 1 é mais barato ");
         }
-        else if (produto1 < produto2 & produto2 < produto3){
-            System.out.println("Produto é mais barato");
+        else if (produto2 < produto1 & produto2 < produto3){
+            System.out.println("Produto2 é mais barato");
         }
         else {
             System.out.println("Produto 3 é mais barato");
