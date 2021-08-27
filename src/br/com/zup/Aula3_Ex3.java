baa0f9fdb93e00e0be5e43fb9ec0f38fa0243347
Raline Silva <ralinesilva1@gmail.com>
2021-08-27 09:41:27 -0300

    em resolução

---
+++ src/br/com/zup/Aula3_Ex3.java
@@ -0,0 +1,29 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Aula3_Ex3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Insira a nota 1: ");
+        double nota1 = leitor.nextDouble();
+        System.out.println("Insira a nota 2: ");
+        double nota2 = leitor.nextDouble();
+
+        double media = (nota1 + nota2) / 2;
+
+        if (media >= 7 & media <= 10){
+            System.out.println("Aprovado");
+        }
+        else if (media < 7 & media <= 0 ){
+            System.out.println("Reprovado");
+        }
+        else if (media == 10) {
+            System.out.println("Aprovado com distinção");
+        }
+        else {
+            System.out.println("Insira um número válido");
+        }
+    }
+}
